5616db0dc0b0568edd61dae2afb2f856bf7de16f
Catharina Menezes <cathy.c@hotmail.com>
2020-07-28 11:56:19 -0300

    Commit paginas

--- src/index.js
+++ src/index.js
@@ -3,11 +3,25 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
+import {BrowserRouter, Switch, Route} from 'react-router-dom';
+import cadastroVideo from './pages/cadastro/Video'
+import cadastroCategoria from './pages/cadastro/Categoria';
+
+const Pagina404 =() => ( <div>Página não encontrada</div> )
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+   <Switch>
+     <Route path="/" component={App} exact  />
+     <Route path="/cadastro/video" component={cadastroVideo}  />
+     <Route path="/cadastro/categoria" component={cadastroCategoria}  />
+     <Route  component={Pagina404} />
+   </Switch>
+  </BrowserRouter>,
   document.getElementById('root')
 );
 
 
+//<React.StrictMode>
+//<App />
+//</React.StrictMode>,
